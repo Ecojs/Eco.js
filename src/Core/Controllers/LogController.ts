6cc822b0ef2980ac4e094460b5b209952315ea20
Ethan Snow <bombitmanbomb@gmail.com>
2023-04-25 22:02:20 -0400

    add Log retrieval

--- src/Core/Controllers/LogController.ts
+++ src/Core/Controllers/LogController.ts
@@ -5,7 +5,10 @@ export class LogController extends ControllerBase {
   constructor(client: ECO) {
     super(client)
   }
-  public async list(category?: string) {
+  public async list(category: string = '') {
     return this.GET<string[], string[]>(`/api/v1/logs/${category}`)
+  }
+  public async getLog(log: string) {
+    return this.GET<string[], string[]>(`/api/v1/logs/get?filepath=${log}`)
   }
 }
