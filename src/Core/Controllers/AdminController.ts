621f6378aa01466696a6af33a447b1850bea1b89
Ethan Snow <bombitmanbomb@gmail.com>
2023-04-25 21:08:27 -0400

    map Admin api

--- src/Core/Controllers/AdminController.ts
+++ src/Core/Controllers/AdminController.ts
@@ -1,3 +1,5 @@
+import { AdminReturnModel, IAdminReturnModel } from '../../structures/Eco/WebServer/Web/Models/AdminReturnModel';
+import { ExportGameModel, IExportGameModel } from '../../structures/Eco/WebServer/Web/Models/ExportGameModel';
 import ECO from '../ECO';
 import { ControllerBase } from './ControllerBase';
 
@@ -8,7 +10,21 @@ export class AdminController extends ControllerBase {
   public async getServername() {
     return this.GET<string, string>('/api/v1/admin/get/servername')
   }
+  public async setServername(name: string) {
+    return this.POST<string, string, void>(`/api/v1/admin/set/servername?name=${name}`, undefined)
+  }
   public async getAccess() {
     return this.GET<string, string>('/api/v1/admin/get/access')
   }
+  public async setAccess(visibility: 'public'): Promise<string>
+  public async setAccess(visibility: 'hidden'): Promise<string>
+  public async setAccess(visibility: 'private', password: string): Promise<string>
+  public async setAccess(visibility: 'public' | 'private' | 'hidden', password?: string): Promise<string> {
+    let query = `?value=${visibility}`
+    if (visibility == 'private') query += `&password=${password}`
+    return this.POST<string, string, void>(`/api/v1/admin/set/access${query}`, undefined);
+  }
+  // public async export() {
+  //   return this.POST<AdminReturnModel, IAdminReturnModel>('/api/v1/admin/game/export', undefined, AdminReturnModel);
+  // }
 }
