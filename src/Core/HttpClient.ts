f15a0780a422a8a58ec9c364654907c7575f08df
Ethan Snow <bombitmanbomb@gmail.com>
2023-04-25 22:01:42 -0400

    add a Debug mode

--- src/Core/HttpClient.ts
+++ src/Core/HttpClient.ts
@@ -6,13 +6,16 @@ import { ClientObjectBase } from '../structures/ClientObjectBase'
 export type HttpClientOptions = {
   base_url?: string
   api_key?: string
+  debug?: boolean
 }
 
 export class HttpClient extends ClientObjectBase {
   private BASE_URL: string
   public api_key?: string
+  public debug: boolean
   constructor(client: ECO, options: HttpClientOptions) {
     super(client)
+    this.debug = options.debug ?? false
     this.BASE_URL = options.base_url ?? 'http://127.0.0.1:3001/'
     Object.defineProperty(this, 'api_key', { value: options.api_key, enumerable: false })
   }
@@ -25,18 +28,24 @@ export class HttpClient extends ClientObjectBase {
   public async fetch<T, V>(method: 'GET' | 'POST', endpoint: string, constructor?: (new (client: ECO, data: V) => T) | ((client: ECO, data: V) => T), body?: any): Promise<T> {
     const headers = this.api_key ? { 'X-API-Key': this.api_key } : undefined
 
-    const request = await fetch(
-      join(this.BASE_URL, endpoint),
-      {
-        method,
-        headers: { 'content-type': 'application/json', 'Accept': 'application/json', ...headers },
-        body
-      })
-    if (request.ok) {
-      if (request.headers.get('Content-Type')?.includes('text/')) {
-        return request.text() as any
+    const init = {
+      method,
+      headers: { 'content-type': 'application/json', 'Accept': 'application/json', ...headers },
+      body
+    }
+    const url = join(this.BASE_URL, endpoint)
+    const response = await fetch(
+      url, init
+    )
+    if (this.debug){
+      console.info(method, url, body)
+      console.info(response.status)
+    }
+    if (response.ok) {
+      if (response.headers.get('Content-Type')?.includes('text/')) {
+        return response.text() as any
       }
-      let data = await request.json()
+      let data = await response.json()
       if (constructor != null)
         try {
           return new (constructor as any)(this.client, data);
@@ -45,6 +54,6 @@ export class HttpClient extends ClientObjectBase {
         }
       return data as T
     }
-    return Promise.reject(request);
+    return Promise.reject(response);
   }
 }
