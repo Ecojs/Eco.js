5e74c67da70aed141503b9da6afe42f6c10ddc40
Ethan Snow <bombitmanbomb@gmail.com>
2023-04-25 21:07:48 -0400

    add POST body type

--- src/Core/Controllers/UsersController.ts
+++ src/Core/Controllers/UsersController.ts
@@ -1,8 +1,12 @@
+import { IUser, User } from '../../structures/Eco/Web/Core/DataTransferObjects/V1/User';
 import ECO from '../ECO';
 import { ControllerBase } from './ControllerBase';
 
 export class UsersController extends ControllerBase {
   constructor(client: ECO) {
     super(client)
+  }
+  public async getUsers(hoursPlayedGte: number = 0) {
+    return this.GET<User[], IUser[]>(`/api/v1/users?hoursPlayedGte=${hoursPlayedGte}`, (client, data) => data.map(v => new User(client, v)))
   }
 }
