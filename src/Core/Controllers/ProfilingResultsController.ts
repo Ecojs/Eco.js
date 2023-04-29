2fcde6693fe01341dd5f89f6a29200d603fe2029
bombitmanbomb <bombitmanbomb@gmail.com>
2023-04-29 20:00:03 +0000

    Deploying to gh-pages from @ afocommunity/eco.js@e96bcf99e8b02c07520db23677cfad7e061bb644 🚀

--- src/Core/Controllers/ProfilingResultsController.ts
+++ src/Core/Controllers/ProfilingResultsController.ts
@@ -1,8 +1,24 @@
+import {
+  IProfilingResult,
+  ProfilingResult,
+} from "../../structures/Eco/Web/Core/DataTransferObjects/V1/ProfilingResult";
 import ECO from "../ECO";
 import { ControllerBase } from "./ControllerBase";
 
 export class ProfilingResultsController extends ControllerBase {
   constructor(client: ECO) {
     super(client);
+  }
+
+  public async getProfilingResults() {
+    return this.GET<ProfilingResult[], IProfilingResult[]>(
+      `/api/v1/profiling-results`,
+      (client, results) =>
+        results.map((result) => new ProfilingResult(client, result))
+    );
+  }
+
+  public async getProfile(filename: string) {
+    return this.GET<any, any>(`/api/v1/profiling-results/${filename}`);
   }
 }
