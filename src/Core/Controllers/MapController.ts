71979fb29ec85805252e846f5ea0afab229f9e93
bombitmanbomb <bombitmanbomb@gmail.com>
2023-04-29 19:53:41 +0000

    Deploying to gh-pages from @ afocommunity/eco.js@b5abfd824a86b5b0089e1aae3566d19c21623160 🚀

--- src/Core/Controllers/MapController.ts
+++ src/Core/Controllers/MapController.ts
@@ -8,6 +8,18 @@ import {
   Entity,
   IEntity,
 } from "../../structures/Eco/Web/Core/DataTransferObjects/V1/Entity";
+import { IVector3i, Vector3i } from "../../structures/Eco/Shared/Math/Vector3i";
+import { IVector2i } from "../../structures/Eco/Shared/Math/Vector2i";
+
+type MapJson = {
+  WaterLevel: number;
+  DistrictSuffix: string;
+  WorldTime: number;
+  LayerNames: string[];
+  Plots: {
+    [key: string]: IVector2i[];
+  };
+};
 
 export class MapController extends ControllerBase {
   constructor(client: ECO) {
@@ -19,13 +31,28 @@ export class MapController extends ControllerBase {
   public async getEntityTypes() {
     return this.GET<string[], string[]>("/api/v1/map/entitytypes");
   }
-  public async getEntities() {
+  /**
+   * Endpoint not properly set up. Only fetches Players currently
+   */
+  public async getEntities(entityTypes = ["Player"]) {
     return this.GET<Entity[], IEntity[]>(
-      "/api/v1/map/entities",
+      `/api/v1/map/entities?entityTypes?=${entityTypes}`,
       (c: ECO, d: IEntity[]) => d.map((v) => new Entity(c, v))
     );
   }
+  public async getWorldDimension() {
+    return this.GET<Vector3i, IVector3i>(`/api/v1/map/dimension`, Vector3i);
+  }
+  public async getLayerList() {
+    return this.GET<string[], string[]>(`/api/v1/map/layerList`);
+  }
+  public async getWebMapData() {
+    return this.GET<MapJson, MapJson>(`/api/v1/map/map.json`);
+  }
   public async getWaterLevel() {
     return this.GET<number, number>("/api/v1/map/waterLevel");
+  }
+  public async getProperty() {
+    return this.GET<unknown, unknown>("/api/v1/map/property");
   }
 }
