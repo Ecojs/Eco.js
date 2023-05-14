8cda60e79cd564a9f63a934d3cb63e5dcdfb1cdd
bombitmanbomb <bombitmanbomb@gmail.com>
2023-05-14 19:37:25 +0000

    Deploying to gh-pages from @ Ecojs/Eco.js@c2a35d7451e85fe13464d5b18fcabd0f424218e6 🚀

--- src/structures/Eco/Web/Core/DataTransferObjects/V1/PluginInfo.ts
+++ src/structures/Eco/Web/Core/DataTransferObjects/V1/PluginInfo.ts
@@ -18,6 +18,8 @@ export class PluginInfo extends ClientObjectBase implements IPluginInfo {
     this.HasConfig = $b.HasConfig;
   }
   public getConfig() {
+    if (this.HasConfig == false)
+      throw new Error(`${this.TypeName} does not have a Config.`);
     if (this.TypeName == null)
       throw new Error('TypeName cannot be null to fetch the config.');
     return this.client.plugins.getPluginConfig(this.TypeName as string);
