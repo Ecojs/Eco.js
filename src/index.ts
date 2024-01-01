f59bada4bee4ce9957a7c8f262a8c7e3d598a6e1
Ethan Snow <bombitmanbomb@gmail.com>
2024-01-01 16:10:08 -0500

    Create text util functions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * as Plugins from './Plugins/index.js';
 export { default as ECO } from './Core/ECO.js';
 export * as Controllers from './Core/Controllers/index.js';
 export * as DTO from './Core/Shared/index.js';
+export * as TextUtils from './Core/Utils/TextUtils.js';
