d39fc037afa97e51e2f205be16ce24763f71b5e2
Ethan Snow <bombitmanbomb@gmail.com>
2023-12-31 22:03:01 -0500

    Update DataExportController.ts

--- src/Core/Controllers/DataExportController.ts
+++ src/Core/Controllers/DataExportController.ts
@@ -5,4 +5,34 @@ export class DataExportController extends ControllerBase {
   constructor(client: ECO) {
     super(client);
   }
+
+  public async species(species: string) {
+    return this.GET<string, string>(
+      `/api/v1/exporter/species?speciesName=${species}`,
+    );
+  }
+  public async environment(
+    category: string,
+    units: string = 'PPM',
+    column: string = 'Climate',
+  ) {
+    return this.GET<string, string>(
+      `/api/v1/exporter/environment?category=${category}&units=${units}&column=${column}`,
+    );
+  }
+  public async actions(actionName: string) {
+    return this.GET<string, string>(
+      `/api/v1/exporter/actions?actionName=${actionName}`,
+    );
+  }
+
+  public async speciesList() {
+    return this.GET<string, string>(`/api/v1/exporter/speciesList`);
+  }
+  public async environmentList() {
+    return this.GET<string, string>(`/api/v1/exporter/environmentList`);
+  }
+  public async actionList() {
+    return this.GET<string, string>(`/api/v1/exporter/actionList`);
+  }
 }
